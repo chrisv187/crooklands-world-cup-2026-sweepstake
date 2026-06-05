e470dd794ead8a4d57203406a2bb42dfec52e150
chrisv187 <chris@crooklands.com>
2026-06-05 15:11:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -44,10 +44,7 @@ await new Promise(r=>setTimeout(r,1000));
 if(hiddenReveal){
 
     document.getElementById('presentation').innerHTML = `
-    confetti({
-    particleCount: 120,
-    spread: 90,
-    origin: { y: 0.6 }
+
 });
     <div style="font-size:3rem;font-weight:bold;">
         ${names[i]}
